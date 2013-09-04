d17b973f53e11866f7417d6386615de548f0657b
Jimmi <jamescarney10@gmail.com>
2013-09-04 10:49:00 -0500

    added initial fields to models/migrations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+  has_many :tags
+  has_many :categories, through: :tags
+  belongs_to :user
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :article
+
 end
