261e86f328f52e8a3f0e88811a489dba6b345bfd
Jimmi <jamescarney10@gmail.com>
2013-09-06 11:01:46 -0500

    updated main/article controllers, adding filter to article model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,11 @@
 class MainController < ApplicationController
   def index
-    @categories = Category.all
+    @cities = City.all
   end
+  def filter
+    filter_hash = {city: params["city"], writer: params["writer"], categories: params['category'], writer_type: params["writer_type"]}
 
+    @articles = Article.filter(filter_hash)
+    render json: {articles: @articles}
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,20 @@ class Article < ActiveRecord::Base
   has_many :itineraries
   has_many :cities, through: :itineraries
   belongs_to :user
+
+  def self.filter(hash)
+    hash.delete_if {|k,v| v == ""}
+    keys = []
+    hash.each {|k,v| keys << k } 
+    p Article.find_by_city(hash[:city])
+  end
+
+  def self.find_by_city(name)
+    cities = City.where(name: name)
+    articles = []
+    cities.each {|city| articles << city.articles} if cities != nil
+    articles
+  end
+
+  def self.find_by_writer_type(id)
 end
