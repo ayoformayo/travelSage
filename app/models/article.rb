8f14862304220983e22e8ab6740fa93132e55ef6
Jimmi <jamescarney10@gmail.com>
2013-09-06 16:53:31 -0500

    super early filter

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,18 +6,64 @@ class Article < ActiveRecord::Base
   belongs_to :user
 
   def self.filter(hash)
+    
+    p hash
     hash.delete_if {|k,v| v == ""}
     keys = []
     hash.each {|k,v| keys << k } 
-    p Article.find_by_city(hash[:city])
+    p keys
+    case keys[0]
+      when :city
+        articles = Article.find_by_city(hash[:city])
+      when :writer_type
+        articles = Article.find_by_writer_type(hash[:writer_type].to_i)
+      when :categories
+       articles =  Article.find_by_categories(hash[:categories])
+      when :writer
+        articles = Article.find_by_writer(hash[:writer])
+    end
+    keys.shift
+    new_array = []
+    keys.each do |key|
+      case key
+        when :city
+          city_names = []
+          article.cities.each {|city| city_names << city.name}
+          articles.delete_if{|article| !city_names.include?(hash[key])}
+        when :writer_type
+          articles.delete_if{|article| article.user.status != hash[key].to_i}
+        when :writer
+          articles.delete_if {|article| article.user.username != hash[key]}
+      end        
+    end
+  
+    p articles
+    
   end
 
   def self.find_by_city(name)
     cities = City.where(name: name)
     articles = []
     cities.each {|city| articles << city.articles} if cities != nil
-    articles
+    articles.flatten!
   end
 
   def self.find_by_writer_type(id)
+    articles = []
+    users = User.where(status: id)
+    users.each {|user| articles << user.articles} if users
+    articles.flatten!
+    articles
+  end
+
+  def self.find_by_writer(name)
+    user = User.find_by_username(name)
+    articles = user.articles if user
+    articles.flatten!
+    articles
+  end
+
+  def self.find_by_categories(array)
+    
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,6 @@
-# europe = Continent.create(name: "Europe")
-# spain = Country.create(name: "Spain", continent_id: europe.id)
-# barcelona = City.create(name: "Barcelona", country_id: spain.id)
-# australia = Continent.create(name: "Australia")
-# australia_country = Country.create(name: "Australia", continent_id: australia.id)
-# sydney = City.create(name: "Sydney", country_id: australia_country.id)
-# north_a = Continent.create(name: "North America")
-# united_states = Country.create(name: "United States", continent_id: north_a.id)
-# chicago = City.create(name: "Chicago", country_id: united_states.id)
-# south_a = Continent.create(name: "South America")
-# argentina = Country.create(name: "Argentina", continent_id: south_a.id)
-# buenos_aires = City.create(name: "Buenos Aires", country_id: south_a.id)
-# africa = Continent.create(name: "Africa")
-# egypt = Country.create(name: "Egypt", continent_id: africa.id)
-# cairo = City.create(name: "Cairo", country_id: egypt.id)
-# asia = Continent.create(name: "Asia")
-# china = Country.create(name: "China", continent_id: asia.id)
-# beijing = City.create(name: "Beijing", country_id: china.id)
 
 10.times do
-  @user = User.create(username: ShakespeareIpsum::Name.name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  @user = User.create(username: ShakespeareIpsum::Name.name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, status: [0,1].sample)
   3.times do
     @article = Article.create(title: ShakespeareIpsum.insult!, body: ShakespeareIpsum::Sonnets.sonnet, user_id: @user.id)
     2.times do
