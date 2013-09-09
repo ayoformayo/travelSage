76b3fce628b9317b30d36aa29cc148c125da91c2
Jimmi <jamescarney10@gmail.com>
2013-09-09 17:06:15 -0500

    forwards tor results

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,19 @@
-function onSuccess(success){
-  console.log(success);
-}
+// function onSuccess(success){
+//   var newDiv = ""
+ 
+// }
 function toggleFilter(event){
   event.preventDefault();
   $('.filterForm').toggleClass('hidden');
 }
-function submitFilter(event){
-  event.preventDefault();
-  arr = $(this).serializeArray();
-$.post('/filter', arr, onSuccess)
-  // console.log($(this).serializeArray());
-}
+// function submitFilter(event){
+//   // event.preventDefault();
+//   arr = $(this).serializeArray();
+// $.post('/filter', arr, onSuccess)
+//   // console.log($(this).serializeArray());
+// }
 
 $(document).ready(function(){
   $('.filterButton').click(toggleFilter);
-  $('.filterForm').on('submit',submitFilter);
+  // $('.filterForm').on('submit',submitFilter);
 })
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,6 +7,8 @@ class MainController < ApplicationController
     filter_hash = {city: params["city"], writer: params["writer"], category: params['category'], writer_type: params["writer_type"]}
 
     @articles = Article.filter(filter_hash)
-    render json: {articles: @articles}
+    p ">>>>>"
+    p @articles
+    render "results", layout: true
   end
 end
