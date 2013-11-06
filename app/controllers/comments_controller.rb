215e36e7d7e03b12828458fa27e8aaf6ec355cb0
Jimmi Carney <jcarney@urbanbound.com>
2013-11-05 23:06:45 -0600

    comment has votes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
     # fix user_id
     @comment = Comment.create(text: params['comment']['text'], user_id: 11, commentable_type: "Article", commentable_id: article.id)
     # comment.user_id = current_user.id
+    render :partial => 'comment'
 
     # redirect_to article_path(14)
   end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,12 +2,16 @@ class VotesController < ApplicationController
   def create
     @user = User.find(10)
     @vote = Vote.where(voteable_id: params['voteable_id'], voteable_type: params['voteable_type'], user_id: 10).first_or_create
-    @vote.update(value: params["value"])
+    @vote.update_attributes(value: params["value"])
+    @voteable_type = params["voteable_type"]
+    @voteable_id = params["voteable_id"]
+    if @voteable_type == 'Article'
+      @count = Article.find(params['voteable_id']).votes.sum(:value)
+    elsif @voteable_type == 'Comment'
+      @count = Comment.find(params['voteable_id']).votes.sum(:value)
+    end
     @vote.save
-    p @vote
-    p "???????"
-    p @user.votes.sum(:value)
-    redirect_to root_path
+
   end
 
   def update
